d8ac9f20f37caa476ad853c1f6c79411b9d83563
Edgar Dewsbery <edewsbery@gmail.com>
2017-06-07 15:36:03 +0100

    add mockdata tracklists

---
+++ mockdataTracklists.js
@@ -0,0 +1,299 @@
+var mockData = {
+    fourTetBoilerRoom: {
+        title: "Four Tet Boiler Room Set",
+        tracklist: [{
+                "track id_number": 1,
+                "Artist": "Albert Ayler",
+                "Title": "Ghosts"
+            },
+            {
+                "track id_number": 2,
+                "Artist": "Anthony Naples",
+                "Title": "Refugio"
+            },
+            {
+                "track id_number": 3,
+                "Artist": "O'Flynn",
+                "Title": "Tyrion"
+            },
+            {
+                "track id_number": 4,
+                "Artist": "Tarsius",
+                "Title": "Nightlife"
+            },
+            {
+                "track id_number": 5,
+                "Artist": "Aphex Twin",
+                "Title": "XMAS_EVET1 N"
+            },
+            {
+                "track id_number": 6,
+                "Artist": "Percussions",
+                "Title": "Sext"
+            },
+            {
+                "track id_number": 7,
+                "Artist": "Akukaja",
+                "Title": "Benga Benga"
+            },
+            {
+                "track id_number": 8,
+                "Artist": "Arnie Love & The Lovettes",
+                "Title": "We've Had Enough"
+            },
+            {
+                "track id_number": 9,
+                "Artist": "Dem 2",
+                "Title": "Luv’s Hard"
+            },
+            {
+                "track id_number": 10,
+                "Artist": "No Boundaries",
+                "Title": "Modular Pursuits (Daphni Mix)"
+            },
+            {
+                "track id_number": 11,
+                "Artist": "Moses Boyd Exodus",
+                "Title": "Rye Lane Shuffle"
+            },
+            {
+                "track id_number": 12,
+                "Artist": "Joao Donato",
+                "Title": "Cala Boca Menino"
+            },
+            {
+                "track id_number": 13,
+                "Artist": "Alice Clark",
+                "Title": "Don't You Care"
+            },
+            {
+                "track id_number": 14,
+                "Artist": "Connie Laverne",
+                "Title": "Can’t Live Without You"
+            },
+            {
+                "track id_number": 15,
+                "Artist": "The Fatback Band",
+                "Title": "Goin' To See My Baby"
+            },
+            {
+                "track id_number": 16,
+                "Artist": "Bobby McFerrin",
+                "Title": "Thinkin' About Your Body (Joe Edit)"
+            },
+            {
+                "track id_number": 17,
+                "Artist": "Martyn & Four Tet",
+                "Title": "Glassbeadgames (8 Hours At Fabric Dub)﻿"
+            },
+            {
+                "track id_number": 18,
+                "Artist": "Jo Bisso",
+                "Title": "Love Somebody﻿"
+            },
+            {
+                "track id_number": 18,
+                "Artist": "Hu Vibrational",
+                "Title": "Locomotion﻿"
+            },
+            {
+                "track id_number": 18,
+                "Artist": "Emanative & Ahmed Abdullah",
+                "Title": "Lions Of Judah (Four Tet Re-Edit)﻿"
+            },
+            {
+                "track id_number": 18,
+                "Artist": "Mavis John",
+                "Title": "Use My Body﻿"
+            },
+            {
+                "track id_number": 18,
+                "Artist": "Jan Hammer Group",
+                "Title": "Don't You Know﻿"
+            }
+        ]
+    },
+    nightmaresOnWaxBoilerRoom: {
+        title: "Nightmares On Wax Boiler Room Set",
+        tracklist: [{
+                "track id_number": 1,
+                "Artist": "Sylvia Robinson",
+                "Title": "Sweet Stuff"
+            },
+            {
+                "track id_number": 2,
+                "Artist": "The Groovers",
+                "Title": "Where You Belong"
+            },
+            {
+                "track id_number": 3,
+                "Artist": "Nightmares on Wax",
+                "Title": "Be, I Do(The Plantyherbs Remix)"
+            },
+            {
+                "track id_number": 4,
+                "Artist": "Ed Wizard & Disco Double Dee",
+                "Title": "Straight Up"
+            },
+            {
+                "track id_number": 5,
+                "Artist": "Quadron",
+                "Title": "LFT (Leighton Hema edit)"
+            },
+            {
+                "track id_number": 6,
+                "Artist": "Nautilis",
+                "Title": "Nautilis"
+            },
+            {
+                "track id_number": 7,
+                "Artist": "Diana Ross",
+                "Title": "It's My House"
+            },
+            {
+                "track id_number": 8,
+                "Artist": "YSE Saint Laur'ant",
+                "Title": "Warm Wind Brewing"
+            },
+            {
+                "track id_number": 9,
+                "Artist": "Nightmares on Wax",
+                "Title": "Now Is The Time"
+            },
+            {
+                "track id_number": 10,
+                "Artist": "Wetty Bright",
+                "Title": "Slip & Do It (V Feels So Good edit)"
+            },
+            {
+                "track id_number": 11,
+                "Artist": "Ray Parker Jr.",
+                "Title": "Just Havin' Fun (Hober Mallow Edit)"
+            },
+            {
+                "track id_number": 12,
+                "Artist": "Typesun",
+                "Title": "Last Home (Dj Nature Remix)"
+            },
+            {
+                "track id_number": 13,
+                "Artist": "Chromeo x Joe Bataan x Boz Scaggs",
+                "Title": "Hot Mess (Lowdown Party Mix)"
+            },
+            {
+                "track id_number": 14,
+                "Artist": "Janet Jackson",
+                "Title": "If (Kaytranada Remix)"
+            },
+            {
+                "track id_number": 15,
+                "Artist": "Mofongo Boogaloo Descarga",
+                "Title": "Funk Ferret"
+            },
+            {
+                "track id_number": 16,
+                "Artist": "Berimbau",
+                "Title": "Dario & Dersu"
+            },
+            {
+                "track id_number": 17,
+                "Artist": "Alma Negra",
+                "Title": "Mao Negra﻿"
+            },
+            {
+                "track id_number": 18,
+                "Artist": "Nighmares On Wax",
+                "Title": "Mao Negra﻿"
+            },
+            {
+                "track id_number": 19,
+                "Artist": "Titenots",
+                "Title": "Wordy"
+            }
+        ]
+    },
+    joseyRebelleBoilerRoom: {
+        title: "Josey Rebelle Boiler Room",
+        tracklist: [{
+                "track id_number": 1,
+                "Artist": "Calibre",
+                "Title": "Archive"
+            },
+            {
+                "track id_number": 2,
+                "Artist": "Studio 2",
+                "Title": "Dirty Games"
+            },
+            {
+                "track id_number": 3,
+                "Artist": "Rufige Kru",
+                "Title": "Menace"
+            },
+            {
+                "track id_number": 4,
+                "Artist": "Wots My Code",
+                "Title": "Dubplate"
+            },
+            {
+                "track id_number": 5,
+                "Artist": "Noise Factory",
+                "Title": "Can You Feel The Rush"
+            },
+            {
+                "track id_number": 6,
+                "Artist": "Remarc & Lewi Cifer",
+                "Title": "Ricky"
+            },
+            {
+                "track id_number": 7,
+                "Artist": "Dillinja",
+                "Title": "Muthafucka"
+            },
+            {
+                "track id_number": 8,
+                "Artist": "DJ Die",
+                "Title": "Drop Bear"
+            },
+            {
+                "track id_number": 9,
+                "Artist": "Souljah",
+                "Title": "Fade II Black"
+            },
+            {
+                "track id_number": 10,
+                "Artist": "Blackstar",
+                "Title": "Champion DJ"
+            },
+            {
+                "track id_number": 11,
+                "Artist": "Blackstar",
+                "Title": "Kunta Kinte"
+            },
+            {
+                "track id_number": 12,
+                "Artist": "Dillinja",
+                "Title": "Violent Killa"
+            },
+            {
+                "track id_number": 13,
+                "Artist": "Ed Rush & Optical",
+                "Title": "Cutslo (Lokuste Mix)"
+            },
+            {
+                "track id_number": 14,
+                "Artist": "Digital",
+                "Title": "Deadline"
+            },
+            {
+                "track id_number": 15,
+                "Artist": "The Terrorist",
+                "Title": "The Chopper"
+            },
+            {
+                "track id_number": 16,
+                "Artist": "Marcus Intalex & ST Files",
+                "Title": "How You Make Me Feel﻿"
+            }
+        ]
+    }
+};
